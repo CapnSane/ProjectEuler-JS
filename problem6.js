9fef4a2608d120696539fcade82cc8fd2871a496
Leonardo Schneider <lnrdschneider@gmail.com>
2022-01-12 13:52:16 -0300

    problem 6 solved using javascript

---
+++ problem6.js
@@ -0,0 +1,42 @@
+// Problem 6
+
+// The sum of the squares of the first ten natural numbers is,
+
+// 1**2 + 2**2 + ... + 10**2 = 385
+
+// The square of the sum of the first ten natural numbers is,
+
+// (1 + 2 + ... + 10)**2 = 55**2 = 3025
+
+// Hence the difference between the sum of the squares of the
+// first ten natural numbers and the square of the sum is
+
+// 3025 - 285 = 2640.
+
+// Find the difference between the sum of the squares of the
+// first one hundred natural numbers and the square of the sum.
+
+const length = 100;
+
+function sumSquares(length) {
+  let sum = 0;
+  for (let i = 0; i < length + 1; i++) {
+    sum += i ** 2;
+  }
+  return sum;
+}
+
+function squareSum(length) {
+  let sum = 0;
+  for (let i = 0; i < length + 1; i++) {
+    sum += i;
+  }
+  return sum ** 2;
+}
+
+function difference() {
+  let result = squareSum(length) - sumSquares(length);
+  return result;
+}
+
+console.log(difference());
